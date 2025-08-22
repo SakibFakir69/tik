9d4eb126f0b757704accc3c9d08e557d3c02020a
Sakib Fakir <FakirSakib22232@gmail.com>
2025-08-22 18:34:28 +0600

    removed exoprt

--- middleware/rate-limiting.js
+++ middleware/rate-limiting.js
@@ -3,7 +3,7 @@
 
 const rateLimit = require("express-rate-limit"); // use require instead of import
 
-export const limiter = rateLimit({
+const limiter = rateLimit({
   windowMs: 1 * 60 * 1000, // 1 minute
   max: 5,                   // Limit each IP to 5 requests per windowMs
   standardHeaders: "draft-8", // Send rate limit info in the RateLimit-* headers
